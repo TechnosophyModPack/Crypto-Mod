8be597732d30471b7754c435014334c656ce9790
Technosophy <mercuriouszoldyck@gmail.com>
2019-05-27 07:05:02 -0700

    Initial Commit

---
+++ src/main/java/common/CryptoMod.java
@@ -0,0 +1,81 @@
+package common;
+
+import org.apache.logging.log4j.Logger;
+
+import common.item.ItemHardwareWallet;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod(modid = CryptoMod.MODID, name=CryptoMod.NAME)
+@Mod.EventBusSubscriber
+public class CryptoMod
+{
+    public static final String NAME="CryptoMod";
+    public static final String MODID="cryptomod";
+    
+    public static Logger logger;
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("Initializing Cryptocurrency M@nod");
+    }
+
+    public static CreativeTabs TAB = new CreativeTabs("cryptomodtab")
+    {
+        @Override
+        public ItemStack createIcon()
+        {
+            return new ItemStack(ModItems.HARDWARE_WALLET);
+        }
+    };
+
+    @SubscribeEvent
+    public static void onItemRegister(RegistryEvent.Register<Item> event) {
+
+        event.getRegistry().registerAll(
+                new ItemHardwareWallet().setTranslationKey("hardware_wallet").setRegistryName("hardware_wallet").setCreativeTab(TAB)
+        );
+
+    }
+
+/*    @ObjectHolder(value = "")
+    public static class ModBlocks {
+    	public static final Block BITCOIN_MINER_BLOCK = new BitcoinMinerBlock();
+    } 
+*/
+    
+    @ObjectHolder(MODID)
+    public static class ModItems {
+        public static final Item HARDWARE_WALLET = null;
+    }
+
+    @Mod.EventBusSubscriber(value= Side.CLIENT, modid=MODID)
+    public static class ClientEvents
+    {
+        @SubscribeEvent
+        public static void onRegisterModels(ModelRegistryEvent event)
+        {
+            ModelLoader.setCustomModelResourceLocation(ModItems.HARDWARE_WALLET, 0, new ModelResourceLocation(ModItems.HARDWARE_WALLET.getRegistryName(), "inventory"));
+        }
+    }
+}
---
+++ src/main/java/common/item/ItemHardwareWallet.java
@@ -0,0 +1,79 @@
+package common.item;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import common.CryptoMod;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemHardwareWallet extends Item  {
+	
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+	{
+		
+		if(!worldIn.isRemote) 
+		{
+			ItemStack stack = playerIn.getHeldItem(handIn);
+			NBTTagCompound nbt = stack.getTagCompound();
+			if(nbt == null) { nbt = new NBTTagCompound(); }
+			if(!nbt.hasKey("bitcoins")) 
+			{ 
+				nbt.setInteger("bitcoins", 0);
+				if (stack.getCount() > 1 ) 
+				{
+					stack.shrink(1);
+					ItemStack copiedStack = stack.copy();
+					copiedStack.shrink(copiedStack.getCount() - 1);
+					copiedStack.setTagCompound(nbt);
+					if(!playerIn.inventory.addItemStackToInventory(copiedStack)) 
+					{
+						playerIn.dropItem(copiedStack, false);
+					}
+				}
+				else { stack.setTagCompound(nbt); }
+				
+				//nbt.setInteger("bitcoins", nbt.getInteger("bitcoins") + 1); 
+			}
+
+			CryptoMod.logger.info(nbt.getInteger("bitcoins"));
+		}
+		
+		return new ActionResult(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+		
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+		final NBTTagCompound compound = stack.getTagCompound();
+		
+		if (stack.getTagCompound() != null && compound.hasKey("bitcoins")) { tooltip.add("Bitcoins: " + stack.getTagCompound().getInteger("bitcoins")); }
+    }
+	
+	@Override
+	public int getItemStackLimit(ItemStack stack) { return (stack.hasTagCompound()) ? 1 : 16;}
+	
+	@Override
+	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+		
+		//CryptoMod.logger.info("Ticking Item");
+		
+	}
+	
+}
